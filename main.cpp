b0c662bb38d84567669df8d232b5e3949912f49f
byliutao <704872493@qq.com>
2023-07-06 02:57:44 +0800

    remove build

--- main.cpp
+++ main.cpp
@@ -2,8 +2,16 @@
 #include <opencv2/opencv.hpp>
 #include <graph.h>
 using namespace std;
+using namespace cv;
+
+void test(Mat &img, Rect &interst_area){
+
+}
+
 
 int main(){
+    Mat test_img = imread("./data/fox.jpg");
+    imshow("test_img",test_img);
     cout<<"hello"<<endl;
     return 0;
 }
